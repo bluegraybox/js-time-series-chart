a855f1e56f4e28f044bbae585b24caf65a046cfc
Colin MacDonald <colin.r.macdonald@gmail.com>
2018-11-29 09:00:27 -0500

    Clean up realtime code
    Extract hardcoded value to constants.
    Remove unnecessary/redundant function calls.
    Add comments.

--- realtime.js
+++ realtime.js
@@ -1,8 +1,15 @@
-var MAX_POINTS = 1000;
+/* Display a graph of historical values retrieved from the realtime.cgi service.
+ * Clicking anywhere on the page pauses/resumes the requests to the server.
+ */
+
+var SERVICE_URL = 'http://localhost/~colin/jsgraph/realtime.cgi';
+var MAX_POINTS = 1000;  // How much history we keep
+var UPDATE_INTERVAL = 250;  // Delay between updates, in milliseconds
+
 var timer;
 var series = [];
 var options = {
-  xaxis: { mode: "time", timeformat: "%H:%M:%S" },
+  xaxis: { mode: "time", timeformat: "%M:%S" },  // only show minutes & seconds
   yaxis: { min: 0 },
   grid: { hoverable: true }
 };
@@ -22,18 +29,20 @@ function hover(event, pos, item) {
   }
 };
 
-function addData(text) {
-  series.push([(new Date()).getTime(), text]);
+// Callback function which adds retrieved value to time series.
+function addData(response) {
+  var timestamp = (new Date()).getTime();
+  series.push([timestamp, response]);
   plotSeries();
   while (series.length > MAX_POINTS) {
-    // clearTimeout(timer);
     series.shift();
   }
 };
 
+// Fetch data from web service
 function update() {
   $.ajax({
-    url: 'http://localhost/~colin/jsgraph/realtime.cgi',
+    url: SERVICE_URL,
     jsonp: 'addData',
     dataType: 'jsonp',
     success: function(response) {
@@ -50,10 +59,12 @@ function plotSeries() {
   var p = $.plot(placeholder, [series], options);
 }
 
+// Start timer to periodically fetch data.
 function startTimer() {
-  timer = setInterval(update, 250);
+  timer = setInterval(update, UPDATE_INTERVAL);
 }
 
+// Pause or resume fetching of data
 function toggleTimer() {
   if (timer) {
     clearTimeout(timer);
@@ -69,8 +80,8 @@ $(function () {
   // call our hover function on plothover events
   var placeholder = $("#placeholder");
   placeholder.bind("plothover", hover);
-  update();
-  plotSeries();
-  window.addEventListener("click", toggleTimer);
+
+  addEventListener("click", toggleTimer);
+
   timer = setInterval(update, 250);
 });
